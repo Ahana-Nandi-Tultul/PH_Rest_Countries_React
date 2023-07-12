f1669f60e5be282fe54e05c9a84307a823a9a0c4
Ahana-Nandi-Tultul <83442291+Ahana-Nandi-Tultul@users.noreply.github.com>
2023-07-12 13:31:23 +0600

    Loading Countries

---
+++ src/components/Countries/Countries.js
@@ -0,0 +1,17 @@
+import React, { useEffect, useState } from 'react';
+
+const Countries = () => {
+    const [countries, setCountries] = useState([]);
+    useEffect(() => {
+        fetch('https://restcountries.com/v3.1/all')
+        .then(res => res.json())
+        .then(data => console.log(data))
+    },[])
+    return (
+        <div>
+            <h2>Hello from Countries:</h2>
+        </div>
+    );
+};
+
+export default Countries;
